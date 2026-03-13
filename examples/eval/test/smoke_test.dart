29731cdcc47ab41652480c0abead492c80389e8b
Polina Cherkasova <polina.c@live.com>
2026-03-13 08:54:44 -0700

    Add test against agent. (#794)

--- examples/eval/test/smoke_test.dart
+++ examples/eval/test/smoke_test.dart
@@ -2,15 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: avoid_print
+
 import 'package:flutter_test/flutter_test.dart';
 
+import 'test_infra/ai_client.dart';
 import 'test_infra/api_key.dart';
 
 void main() {
   test('test can read api key "$geminiApiKeyName"', () {
     final String key = apiKeyForEval();
     expect(key, isNotEmpty);
-    // ignore: avoid_print
     print('API Key: ${key.substring(0, 1)}...${key.substring(key.length - 1)}');
+  });
+
+  test('test can talk with AI', () async {
+    final aiClient = DartanticAiClient();
+    addTearDown(aiClient.dispose);
+
+    final String result = await aiClient
+        .sendStream('Please, tell me a joke.', history: [])
+        .first;
+    expect(result, isNotEmpty);
+    print('Result: $result');
   });
 }
